080c582b2071079077e8ded1a1bdcb366ebd0993
brunogm2 <brunobgbm002@gmail.com>
2020-12-25 20:31:26 -0300

    Commit Carousel + section 1

--- src/components/PaginaHome/home.js
+++ src/components/PaginaHome/home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import { Header, Menu, Logo, IconeInstagram } from './style';
+import { Header, Menu, Logo, IconeInstagram, ImgCarousel,
+     ContainerSection, Section, ImgSec1, DivSec1} from './style';
+import Carousel from 'react-bootstrap/Carousel'
+
+import testeps6 from "../../assets/testeps6.jpg"
 
 function Home(){
     return(
@@ -12,6 +16,41 @@ function Home(){
             <Menu> Contato </Menu>
             <IconeInstagram/>
         </Header>
+        <Carousel>
+            <Carousel.Item>
+                <ImgCarousel>
+                    <img src={testeps6} alt="imagem slider1" style={{width: '100%', height:'100%'}} />     
+                </ImgCarousel>  
+                <Carousel.Caption style={{width: '400px', height:"200px", marginTop: '-80px'}}>
+                    <h1>Sofisticação em Manutenção</h1>
+                    <p>Revisão e manutenção de seu veiculo com tecnologia de ponta a ponta.</p>
+                </Carousel.Caption> 
+            </Carousel.Item>
+        </Carousel>
+        <ContainerSection>
+            <Section>
+                <ImgSec1>
+                    <img src={testeps6} alt="imagem slider1"/>
+                </ImgSec1>
+                <DivSec1>
+                    <h1>
+                        Nossos valores
+                    </h1>
+                    <p>
+                    Lorem Ipsum is simply dummy text
+                     of the printing and typesetting industry.
+                      Lorem Ipsum has been the industry's standard dummy
+                       text ever since the 1500s, when an unknown printer took a galley of type and
+                     scrambled it to make a type specimen book.
+                      It has survived not only five centuries, but als
+                      text ever since the 1500s, when an unknown printer took a galley of type and
+                     scrambled it to make a type specimen book.
+                      It has survived not only five centuries, but als
+                      
+                    </p>
+                </DivSec1>
+            </Section>
+        </ContainerSection>
     </>
     )
 }
--- src/components/PaginaHome/style.js
+++ src/components/PaginaHome/style.js
@@ -19,6 +19,7 @@ export const Logo = styled.div `
     flex-direction: row;
     align-self: flex-start;
     margin-right: 100px;
+    z-index: 999;
     `;
 
 export const Menu = styled.div `
@@ -37,3 +38,70 @@ export const IconeInstagram = styled(Menu) `
     margin-left: 150px;
 
     `;
+
+export const ImgCarousel = styled.div ` 
+    width: 100%;
+    height: 687px;
+   background-color: red;
+    `;
+
+export const TextCarousel = styled.div ` 
+    width: 150px;
+    height: 150px;
+    
+    /* margin-top: -250px; */
+    background-color: yellow;
+    `;
+
+export const ContainerSection = styled.section ` 
+    width: 100%;
+    height: 500px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.8);
+`;
+
+export const Section = styled.section ` 
+    width: 1100px;
+    height: 95%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    /* background-color: #fff; */
+`;
+
+export const ImgSec1 = styled.div ` 
+    width: 35%;
+    height: 80%;
+    background-color: red;
+    border-radius: 10px;
+    > img{
+        width: 100%;
+        height: 100%;
+        border-radius: 5px;
+    }
+`;
+
+export const DivSec1 = styled.div ` 
+    width: 500px;
+    height: 80%;
+    margin-left: 180px;
+    /* background-color: gray; */
+    display: flex;
+    flex-direction: column;
+    /* justify-content: center; */
+    align-items: center;
+    > h1{
+        font-family: 'Roboto';
+        font-size: 50px;
+        color: yellow;
+        margin-bottom: 30px;
+    }> p{
+        font-size: 20px;
+        color: #fff;
+    }
+`;
+
+   
